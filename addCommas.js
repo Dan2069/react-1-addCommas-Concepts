d6016189e608d25e095baa7f662c8c8de6ca9380
Daniel <danielmosqueda15@gmail.com>
2022-09-19 00:40:25 -0400

    added functionality for decimals and implemented tests

--- addCommas.js
+++ addCommas.js
@@ -2,45 +2,59 @@ function addCommas(num) {
     let all_parts = []
     let text = String(num);
 
-
-
     if (text.includes('.')){
 
         pointPosition= text.indexOf('.');
-
+        // Gets the position of the decimal
+        // Ex. 12,345.678 => 5
         pointRemainder = Math.abs((pointPosition % 3))
-        // console.log(text[pointRemainder])
-        // console.log(pointRemainder);
-
-        int_part = text.slice(0,pointRemainder);
-        // console.log(int_part);
-        all_parts.push(int_part);
+        // Gets the range for the inital part that isn't in sections of 3
+        // Ex. 5 % 3 = 2
 
         end_part = text.slice(pointPosition, text.length);
-        // console.log(end_part);
-
-
+        // Gets the end part of the number
+        // Ex. 12,345.678 => .678
         main_part = text.slice(pointRemainder ,pointPosition);
-        // console.log(main_part);
+        // Gets the main 3-sections part of the number
+        // Ex. 12,345.678 => 345
 
+        int_part = text.slice(0,pointRemainder);
+        // Gets the inital part of the number
+        // Ex. 12,345.678 => 12
+        all_parts.push(int_part);
+        // Pushes the initial part to the empty all_parts array
+        // Ex. all_parts => ['12']
 
-
-
-
-        for (let i = pointRemainder; i <= main_part.length; i++){
+        for (let i = pointRemainder; i <= main_part.length; i++)
+        // Starting at the inital points until the start of end point
+        {
             let segment = text.slice(i, i+3);
-            i += 3;
-            console.log("Line 34");
-            console.log(segment);
+            // Ex. text[2] to text[5] => 345
+            if(segment.includes('.')){
+            // Accounts if the period is included in the 3-sectioned parts
+            // Ex. .78
+                all_parts.push(segment.slice(0,segment.indexOf('.')));
+                //Ex. Pushes nothing since slices from 0,0
+            }else{
+            i += 2;
+            // Pushes the window further down the number, if still in range of the for loop
             all_parts.push(segment);
+            // Pushes the main 3-sectioned parts into the array 
+            // Ex. all_parts => ['12','345']
+            }
         }
 
-
-        // all_parts.push(end_part);
-
+        numWithComma = all_parts.filter(noSpace).join(',');
+        // Joins the int_part and the main_parts into one string w/ a , connecting them
+        // Ex. ['12','345'] => 12,345
+        return numWithComma + end_part
+        // Returns with the end part
+        // Ex. 12,345 + .678 => 12,345.678
 
     } else {
         let remainder = (text.length % 3)
+        // Gets the range for the inital part that isn't in sections of 3
+        // Ex. 5 % 3 = 2
         int_part = text.slice(0,remainder);
         all_parts.push(int_part);
 
@@ -66,5 +80,6 @@ console.log(addCommas(6))
 console.log(addCommas(-10))
 console.log(addCommas(-5678))
 console.log(addCommas(12345.678))
+console.log(addCommas(123456.789))
 console.log(addCommas(-3141592.65))
 module.exports = addCommas;
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,25 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+  test("1,234", () => {
+    expect(addCommas(1234)).toBe("1,234");
+  });
+  test("1,000,000", () => {
+    expect(addCommas(1000000)).toBe("1,000,000");
+  });
+  test("9,876,543,210", () => {
+    expect(addCommas(9876543210)).toBe("9,876,543,210");
+  });
+  test("6", () => {
+    expect(addCommas(6)).toBe("6");
+  });
+  test("-10", () => {
+    expect(addCommas(-10)).toBe("-10");
+  });
+  test("12,345.678", () => {
+    expect(addCommas(12345.678)).toBe("12,345.678");
+  });
+  test("-3,141,592.65", () => {
+    expect(addCommas( -3141592.65)).toBe("-3,141,592.65");
+  });
 });
